0ad3637b7a6e62a0be44dd62526073c04b7d3569
Helder Filipe <hluisfilipe@gmail.com>
2020-12-02 23:26:16 +0000

    First commit

---
+++ src/main/java/com/hf/buyproductsapi/BuyproductsapiApplication.java
@@ -0,0 +1,13 @@
+package com.hf.buyproductsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyproductsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyproductsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hf/buyproductsapi/BuyproductsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hf.buyproductsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyproductsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
