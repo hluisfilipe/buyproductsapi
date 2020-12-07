3c5a162e393594b2d0b0b634f5fd164098e99894
Helder Filipe <hluisfilipe@gmail.com>
2020-12-07 11:26:46 +0000

    Deleting a Categoria

--- src/main/java/com/hf/buyproductsapi/services/CategoriaService.java
+++ src/main/java/com/hf/buyproductsapi/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.hf.buyproductsapi.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.hf.buyproductsapi.domain.Categoria;
 import com.hf.buyproductsapi.repositories.CategoriaRepository;
+import com.hf.buyproductsapi.services.exceptions.DataIntegrityException;
 import com.hf.buyproductsapi.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,7 +22,7 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objecto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);	
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
