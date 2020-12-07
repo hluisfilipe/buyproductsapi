f5a6a1909c36c74b0963e8ab8d9d5bd22518b236
Helder Filipe <hluisfilipe@gmail.com>
2020-12-07 21:36:25 +0000

    PUT DELETE GET to the Cliente

--- src/main/java/com/hf/buyproductsapi/services/ClienteService.java
+++ src/main/java/com/hf/buyproductsapi/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.hf.buyproductsapi.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.hf.buyproductsapi.domain.Cliente;
+import com.hf.buyproductsapi.dto.ClienteDTO;
 import com.hf.buyproductsapi.repositories.ClienteRepository;
+import com.hf.buyproductsapi.services.exceptions.DataIntegrityException;
 
 @Service
 public class ClienteService {
@@ -17,5 +24,38 @@ public class ClienteService {
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElse(null);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
