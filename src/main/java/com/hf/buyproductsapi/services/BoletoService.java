314b63ec0ccc0eeccb3228ca56e7cf1a844754a6
Helder Filipe <hluisfilipe@gmail.com>
2020-12-12 18:30:51 +0000

    Pedido Inserted

---
+++ src/main/java/com/hf/buyproductsapi/services/BoletoService.java
@@ -0,0 +1,20 @@
+package com.hf.buyproductsapi.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.hf.buyproductsapi.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+}
--- src/main/java/com/hf/buyproductsapi/services/PedidoService.java
+++ src/main/java/com/hf/buyproductsapi/services/PedidoService.java
@@ -1,11 +1,17 @@
 package com.hf.buyproductsapi.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.hf.buyproductsapi.domain.ItemPedido;
+import com.hf.buyproductsapi.domain.PagamentoComBoleto;
 import com.hf.buyproductsapi.domain.Pedido;
+import com.hf.buyproductsapi.domain.enums.EstadoPagamento;
+import com.hf.buyproductsapi.repositories.ItemPedidoRepository;
+import com.hf.buyproductsapi.repositories.PagamentoRepository;
 import com.hf.buyproductsapi.repositories.PedidoRepository;
 import com.hf.buyproductsapi.services.exceptions.ObjectNotFoundException;
 
@@ -15,9 +21,41 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 
+	@Autowired
+	private BoletoService boletoService;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
+
+	@Autowired
+	private ProdutoService produtoService;
+
+	@Autowired
+	ItemPedidoRepository itemPedidoRepository;
+
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objecto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			boletoService.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		obj = repo.save(obj);
+		pagamentoRepository.save(obj.getPagamento());
+		for (ItemPedido ip : obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		itemPedidoRepository.saveAll(obj.getItens());
+		return obj;
 	}
 }
