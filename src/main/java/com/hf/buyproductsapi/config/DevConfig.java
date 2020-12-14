d4df4bee2be14aaae15d0aad3c50e6fb249c506c
Helder Filipe <hluisfilipe@gmail.com>
2020-12-14 16:29:51 +0000

    dev profile mysql

---
+++ src/main/java/com/hf/buyproductsapi/config/DevConfig.java
@@ -0,0 +1,32 @@
+package com.hf.buyproductsapi.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.hf.buyproductsapi.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("$spring.jpa.hibernate.ddl-auto")
+	private String strategy;
+
+	@Bean
+	public boolean instanciateDatabase() throws ParseException {
+		if (!"create".contentEquals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
