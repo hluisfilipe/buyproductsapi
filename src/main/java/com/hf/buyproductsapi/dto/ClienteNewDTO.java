a431f160834a49e42bebbcbcef9b1a3b9c631bc8
Helder Filipe <hluisfilipe@gmail.com>
2020-12-16 16:15:11 +0000

    add pass client

--- src/main/java/com/hf/buyproductsapi/dto/ClienteNewDTO.java
+++ src/main/java/com/hf/buyproductsapi/dto/ClienteNewDTO.java
@@ -24,6 +24,9 @@ public class ClienteNewDTO implements Serializable {
 	private String cpfOuCnpj;
 
 	private Integer tipo;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String senha;
 
 	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
@@ -72,6 +75,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCpfOuCnpj(String cpfOuCnpj) {
 		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 	public Integer getTipo() {
